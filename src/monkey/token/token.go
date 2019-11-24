d51b9469f26d42ec24a22882028310606ce1f5d5
Jacob Lambert <jacob.lambert@gmail.com>
2019-11-23 22:33:21 -0500

    Add multi-character token lexing

--- src/monkey/token/token.go
+++ src/monkey/token/token.go
@@ -8,7 +8,7 @@ type Token struct {
 }
 
 const (
-	ILLEGAL   = "ILLAGAL"
+	ILLEGAL   = "ILLEGAL"
 	EOF       = "EOF"
 	IDENT     = "IDENT"
 	INT       = "INT"
@@ -23,3 +23,15 @@ const (
 	FUNCTION  = "FUNCTION"
 	LET       = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
