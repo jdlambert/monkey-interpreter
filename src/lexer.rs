2030763c9830be0c66a90f7ca090c2f8c888e637
Jacob Lambert <jacob.lambert@gmail.com>
2019-11-28 13:32:23 -0600

    Fix formatting

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,154 +1,157 @@
 use crate::token::Token;
 
 pub struct Lexer {
-  input: String,
-  position: usize,
-  read_position: usize,
-  ch: char,
+    input: String,
+    position: usize,
+    read_position: usize,
+    ch: char,
 }
 
 impl Lexer {
-  pub fn new(input: String) -> Self {
-    let mut lexer = Lexer {
-      input,
-      position: 0,
-      read_position: 0,
-      ch: '\0',
-    };
-    lexer.read_char();
-    lexer
-  }
+    pub fn new(input: String) -> Self {
+        let mut lexer = Lexer {
+            input,
+            position: 0,
+            read_position: 0,
+            ch: '\0',
+        };
+        lexer.read_char();
+        lexer
+    }
 
-  pub fn next_token(self: &mut Self) -> Token {
-    self.skip_whitespace();
+    pub fn next_token(self: &mut Self) -> Token {
+        self.skip_whitespace();
 
-    let tok = match self.ch {
-      '=' => Token::Assign,
-      ';' => Token::Semicolon,
-      '(' => Token::Lparen,
-      ')' => Token::Rparen,
-      ',' => Token::Comma,
-      '+' => Token::Plus,
-      '{' => Token::Lbrace,
-      '}' => Token::Rbrace,
-      '\0' => Token::Eof,
-      _ => {
-        if is_letter(self.ch) {
-          return Token::from_ident(self.read_identifier());
-        } else if is_digit(self.ch) {
-          return Token::Int(self.read_number());
+        let tok = match self.ch {
+            '=' => Token::Assign,
+            ';' => Token::Semicolon,
+            '(' => Token::Lparen,
+            ')' => Token::Rparen,
+            ',' => Token::Comma,
+            '+' => Token::Plus,
+            '{' => Token::Lbrace,
+            '}' => Token::Rbrace,
+            '\0' => Token::Eof,
+            _ => {
+                if is_letter(self.ch) {
+                    return Token::from_ident(self.read_identifier());
+                } else if is_digit(self.ch) {
+                    return Token::Int(self.read_number());
+                }
+                Token::Illegal
+            }
+        };
+        self.read_char();
+        tok
+    }
+
+    fn read_char(&mut self) {
+        self.ch = self
+            .input
+            .chars()
+            .nth(self.read_position)
+            .unwrap_or('\u{0}');
+        self.position = self.read_position;
+        self.read_position += 1;
+    }
+
+    fn read_number(&mut self) -> u32 {
+        let position = self.position;
+        while is_digit(self.ch) {
+            self.read_char();
         }
-        Token::Illegal
-      }
-    };
-    self.read_char();
-    tok
-  }
-
-  fn read_char(&mut self) {
-    self.ch = self.input.chars().nth(self.read_position).unwrap_or('\u{0}');
-    self.position = self.read_position;
-    self.read_position += 1;
-  }
-
-  fn read_number(&mut self) -> u32 {
-    let position = self.position;
-    while is_digit(self.ch) {
-        self.read_char();
+        self.input[position..self.position].parse::<u32>().unwrap()
     }
-    self.input[position..self.position].parse::<u32>().unwrap()
-  }
 
-  fn read_identifier(&mut self) -> &str {
-    let position = self.position;
-    while is_letter(self.ch) {
-        self.read_char();
+    fn read_identifier(&mut self) -> &str {
+        let position = self.position;
+        while is_letter(self.ch) {
+            self.read_char();
+        }
+        &self.input[position..self.position]
     }
-    &self.input[position..self.position]
-  }
 
-  fn skip_whitespace(&mut self) {
-    while is_whitespace(self.ch) {
-      self.read_char();
+    fn skip_whitespace(&mut self) {
+        while is_whitespace(self.ch) {
+            self.read_char();
+        }
     }
-  }
 }
 
 fn is_letter(ch: char) -> bool {
-  'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+    'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
 
 fn is_digit(ch: char) -> bool {
-	'0' <= ch && ch <= '9'
+    '0' <= ch && ch <= '9'
 }
 
 fn is_whitespace(ch: char) -> bool {
-  ' ' == ch || '\t' == ch || '\n' == ch || '\r' == ch
+    ' ' == ch || '\t' == ch || '\n' == ch || '\r' == ch
 }
 
 #[cfg(test)]
 mod tests {
-  use crate::token::Token;
-  use crate::lexer::Lexer;
+    use crate::lexer::Lexer;
+    use crate::token::Token;
 
-  #[test]
-  fn next_token() {
-    let input = r#"let five = 5;
-      let ten = 10;
-      
-      let add = fn(x, y) {
-        x + y;
-      };
-      
-      let result = add(five, ten);"#;
+    #[test]
+    fn next_token() {
+        let input = r#"let five = 5;
+            let ten = 10;
+            
+            let add = fn(x, y) {
+              x + y;
+            };
+            
+            let result = add(five, ten);"#;
 
-    let expected = [
-      Token::Let,
-      Token::Ident("five".to_string()),
-      Token::Assign,
-      Token::Int(5),
-			Token::Semicolon,
-			Token::Let,
-      Token::Ident("ten".to_string()),
-      Token::Assign,
-      Token::Int(10),
-			Token::Semicolon,
-			Token::Let,
-			Token::Ident("add".to_string()),
-      Token::Assign,
-			Token::Function,
-			Token::Lparen,
-			Token::Ident("x".to_string()),
-			Token::Comma,
-			Token::Ident("y".to_string()),
-			Token::Rparen,
-			Token::Lbrace,
-			Token::Ident("x".to_string()),
-			Token::Plus,
-			Token::Ident("y".to_string()),
-			Token::Semicolon,
-			Token::Rbrace,
-			Token::Semicolon,
-      Token::Let,
-			Token::Ident("result".to_string()),
-      Token::Assign,
-			Token::Ident("add".to_string()),
-			Token::Lparen,
-      Token::Ident("five".to_string()),
-			Token::Comma,
-      Token::Ident("ten".to_string()),
-			Token::Rparen,
-			Token::Semicolon,
-    ];
+        let expected = [
+            Token::Let,
+            Token::Ident("five".to_string()),
+            Token::Assign,
+            Token::Int(5),
+            Token::Semicolon,
+            Token::Let,
+            Token::Ident("ten".to_string()),
+            Token::Assign,
+            Token::Int(10),
+            Token::Semicolon,
+            Token::Let,
+            Token::Ident("add".to_string()),
+            Token::Assign,
+            Token::Function,
+            Token::Lparen,
+            Token::Ident("x".to_string()),
+            Token::Comma,
+            Token::Ident("y".to_string()),
+            Token::Rparen,
+            Token::Lbrace,
+            Token::Ident("x".to_string()),
+            Token::Plus,
+            Token::Ident("y".to_string()),
+            Token::Semicolon,
+            Token::Rbrace,
+            Token::Semicolon,
+            Token::Let,
+            Token::Ident("result".to_string()),
+            Token::Assign,
+            Token::Ident("add".to_string()),
+            Token::Lparen,
+            Token::Ident("five".to_string()),
+            Token::Comma,
+            Token::Ident("ten".to_string()),
+            Token::Rparen,
+            Token::Semicolon,
+        ];
 
-		let mut lexer = Lexer::new(input.to_owned());
+        let mut lexer = Lexer::new(input.to_owned());
 
-		for expected_token in expected.iter() {
-				let token = lexer.next_token();
+        for expected_token in expected.iter() {
+            let token = lexer.next_token();
 
-        assert_eq!(&token, expected_token);
-        println!("{:?}", token);
-		}
-
-  }
+            assert_eq!(&token, expected_token);
+            println!("{:?}", token);
+        }
+    }
 }
--- src/token.rs
+++ src/token.rs
@@ -1,27 +1,27 @@
 #[derive(Clone, Debug, PartialEq)]
 pub enum Token {
-  Illegal,
-  Eof,
-  Ident(String),
-  Int(u32),
-  Assign,
-  Plus,
-  Comma,
-  Semicolon,
-  Lparen,
-  Rparen,
-  Lbrace,
-  Rbrace,
-  Function,
-  Let,
+    Illegal,
+    Eof,
+    Ident(String),
+    Int(u32),
+    Assign,
+    Plus,
+    Comma,
+    Semicolon,
+    Lparen,
+    Rparen,
+    Lbrace,
+    Rbrace,
+    Function,
+    Let,
 }
 
 impl Token {
-  pub fn from_ident(keyword: &str) -> Token {
-    match keyword {
-        "fn" => Token::Function,
-        "let" => Token::Let,
-        _ => Token::Ident(keyword.to_owned()),
+    pub fn from_ident(keyword: &str) -> Token {
+        match keyword {
+            "fn" => Token::Function,
+            "let" => Token::Let,
+            _ => Token::Ident(keyword.to_owned()),
+        }
     }
-  }
 }
