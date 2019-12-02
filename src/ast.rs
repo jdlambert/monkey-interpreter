9ad8d50700cfefc22a50afb8f2b861a88cb6ddff
Jacob Lambert <jacob.lambert@gmail.com>
2019-12-01 18:55:48 -0600

    [Formatting]

--- src/ast.rs
+++ src/ast.rs
@@ -1,19 +1,17 @@
 #[derive(Debug, PartialEq)]
 pub struct Program {
-  pub statements: Vec<Statement>,
+    pub statements: Vec<Statement>,
 }
 
 #[derive(Debug, PartialEq)]
 pub enum Statement {
-  Let(Expression, Expression),
-  Return(Option<Expression>),
-  Expression(Expression)
+    Let(Expression, Expression),
+    Return(Option<Expression>),
+    Expression(Expression),
 }
 
 #[derive(Debug, PartialEq)]
 pub enum Expression {
-  Identifier(String),
-  IntLiteral(u32),
+    Identifier(String),
+    IntLiteral(u32),
 }
-
-
