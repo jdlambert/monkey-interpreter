88e5429e0374045096f775f0984fa81ad2606f57
Jacob Lambert <jacob.lambert@gmail.com>
2019-12-04 22:58:09 -0500

    Add the most basic objects

---
+++ src/object.rs
@@ -0,0 +1,19 @@
+use std::fmt;
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Object {
+  Null,
+  Integer(i64),
+  Boolean(bool),
+}
+
+use Object::*;
+
+impl fmt::Display for Object {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match self {
+        Null  => write!(f, "{}", null),
+        Integer(value) | Boolean(value) => write!(f, "{}", value),
+    } 
+}
+
